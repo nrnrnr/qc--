9102575fc4069899879d9f871b8f463b0f356a77
Daniel Damian <>
2001-04-19 17:23:45 +0000

    Supposedly less memory-hungry version

--- tdpe/sparcNbe.mli
+++ tdpe/sparcNbe.mli
@@ -1,12 +1,6 @@
 
 
-val reify_reg_or_imm : SparcTrees.M.reg_or_imm -> Tdpe.dynamic
-val reflect_reg_or_imm : Tdpe.dynamic -> SparcTrees.M.reg_or_imm
-val reify_address : SparcTrees.M.address -> Tdpe.dynamic
-val reflect_address : Tdpe.dynamic -> SparcTrees.M.address
-val reify_regaddr : SparcTrees.M.regaddr -> Tdpe.dynamic
-val reflect_regaddr : Tdpe.dynamic -> SparcTrees.M.regaddr
-val reify_t : SparcTrees.M.t -> Tdpe.dynamic
-val reflect_t : Tdpe.dynamic ->
-SparcTrees.M.t
+val reflect_t : Tdpe.dynamic -> SparcTrees.M.t
+val reify_t : SparcTrees.M.t ->
+Tdpe.dynamic
 
