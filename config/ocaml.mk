a73ac9431af4b4bf1d987b5c913fef46afde4c31
Christian Lindig <lindig@gmail.com>
2002-06-06 18:09:46 +0000

    Rules for .pod file refined.

--- config/ocaml.mk
+++ config/ocaml.mk
@@ -52,12 +52,14 @@ POD2HTML =      pod2html
                 nroff -man $prereq > $target
 
 %.1:            %.pod
+                if [ -z "$NAME" ]; then NAME="Quick C-- Tools"; fi
                 $POD2MAN   --release="Version $VERSION"         \
-                           --center=$NAME                       \
+                           --center="$NAME"                     \
                            --section=1 $prereq > $target
 %.5:            %.pod
+                if [ -z "$NAME" ]; then NAME="Quick C-- Tools"; fi
                 $POD2MAN   --release="Version $VERSION"         \
-                           --center=$NAME                       \
+                           --center="$NAME"                     \
                            --section=5 $prereq > $target
 %-man.html:     %.pod
                 $POD2HTML  --title=$NAME $prereq > $target
