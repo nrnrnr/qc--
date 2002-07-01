ed636829ac6177d45f8294d51d76723f875ed1b3
Reuben Olinsky <olinsky@post.harvard.edu>
2002-07-01 15:27:00 +0000

    added files from _C Interfaces and Implementations_ library (Hanson)

---
+++ interp/assert.c
@@ -0,0 +1,6 @@
+static char rcsid[] = "$Id$";
+#include <cii/assert.h>
+const Except_T Assert_Failed = { "Assertion failed" };
+void (assert)(int e) {
+	assert(e);
+}
---
+++ interp/except.c
@@ -0,0 +1,32 @@
+static char rcsid[] = "$Id$";
+#include <stdlib.h>
+#include <stdio.h>
+#include <cii/assert.h>
+#include <cii/except.h>
+#define T Except_T
+#ifdef WIN32
+__declspec(thread)
+#endif
+Except_Frame *Except_stack = NULL;
+void Except_raise(const T *e, const char *file,
+	int line) {
+	Except_Frame *p = Except_stack;
+	assert(e);
+	if (p == NULL) {
+		fprintf(stderr, "Uncaught exception");
+		if (e->reason)
+			fprintf(stderr, " %s", e->reason);
+		else
+			fprintf(stderr, " at 0x%p", e);
+		if (file && line > 0)
+			fprintf(stderr, " raised at %s:%d\n", file, line);
+		fprintf(stderr, "aborting...\n");
+		fflush(stderr);
+		abort();
+	}
+	p->exception = e;
+	p->file = file;
+	p->line = line;
+	Except_stack = Except_stack->prev;
+	longjmp(p->env, Except_raised);
+}
