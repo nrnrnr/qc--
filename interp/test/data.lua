4891f26b35813b58ba17f08161b15b4503b25b84
Reuben Olinsky <olinsky@post.harvard.edu>
2002-07-18 21:02:13 +0000

    new test for initialized data

---
+++ interp/test/data.lua
@@ -0,0 +1,28 @@
+--- ./regr "../interp $SELF" $REGRFLAGS -out $BASE.1 -err $BASE.2
+
+CMM.exports({ "main", "datalabel" })
+
+CMM.section("data")
+   CMM.define_label("datalabel")
+      -- CMM.emit_raddr("main", 0)
+      CMM.data(32, {"0x63082"})
+CMM.end_section()
+
+CMM.section("code")
+   CMM.procedure("main", 0, 0)
+      CMM.push_symbol("datalabel")
+      CMM.fetch(32, "little", 1)
+      CMM.push_arg()
+      CMM.c_call("cmmprint")
+
+      CMM.push_symbol("datalabel")
+      CMM.push_literal("7")
+      CMM.store(32, "little", 1)
+
+      CMM.push_symbol("datalabel")
+      CMM.fetch(32, "little", 1)
+      CMM.push_arg()
+      CMM.c_call("cmmprint")
+
+      CMM.cmm_return(0,0)
+CMM.end_section()
