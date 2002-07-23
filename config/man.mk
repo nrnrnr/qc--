617eb0fc771f0e70896258223c89c6868ab49407
Christian Lindig <lindig@gmail.com>
2002-07-23 21:01:31 +0000

    Mk rules for manual pages factored out into config/man.mk.

---
+++ config/man.mk
@@ -0,0 +1,23 @@
+#
+# Rules for manual pages
+#
+
+POD2MAN =       pod2man
+POD2HTML =      pod2html
+
+%.man:          %.1
+                nroff -man $prereq > $target
+
+%.1:            %.pod
+                if [ -z "$NAME" ]; then NAME="Quick C-- Tools"; fi
+                $POD2MAN   --release="Version $VERSION"         \
+                           --center="$NAME"                     \
+                           --section=1 $prereq > $target
+%.5:            %.pod
+                if [ -z "$NAME" ]; then NAME="Quick C-- Tools"; fi
+                $POD2MAN   --release="Version $VERSION"         \
+                           --center="$NAME"                     \
+                           --section=5 $prereq > $target
+%-man.html:     %.pod
+                $POD2HTML  --title=$NAME $prereq > $target
+
--- config/ocaml.mk
+++ config/ocaml.mk
@@ -21,9 +21,6 @@ OCAMLLEX =      ocamllex
 # OCAMLC_FLAGS=-I $LIBDIR
 # OCAMLO_FLAGS=-I $LIBDIR
 
-POD2MAN =       pod2man
-POD2HTML =      pod2html
-
 # ------------------------------------------------------------------ 
 # rules
 # ------------------------------------------------------------------ 
@@ -47,21 +44,4 @@ POD2HTML =      pod2html
 
 %.sig:          %.ml
                 $OCAMLC $OCAMLC_FLAGS -c -i $stem.ml > $target
-
-%.man:          %.1
-                nroff -man $prereq > $target
-
-%.1:            %.pod
-                if [ -z "$NAME" ]; then NAME="Quick C-- Tools"; fi
-                $POD2MAN   --release="Version $VERSION"         \
-                           --center="$NAME"                     \
-                           --section=1 $prereq > $target
-%.5:            %.pod
-                if [ -z "$NAME" ]; then NAME="Quick C-- Tools"; fi
-                $POD2MAN   --release="Version $VERSION"         \
-                           --center="$NAME"                     \
-                           --section=5 $prereq > $target
-%-man.html:     %.pod
-                $POD2HTML  --title=$NAME $prereq > $target
-
 
