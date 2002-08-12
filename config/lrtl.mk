137853683772b7187faa1f63668d140ee0050358
Christian Lindig <lindig@gmail.com>
2002-08-12 19:06:32 +0000

    Removed dependecy on heap file. There is no universal heap file location.

--- config/lrtl.mk
+++ config/lrtl.mk
@@ -4,15 +4,15 @@
 
 LRTL=lrtl
 
-^($MACHINEREGEX).ml'$' ($MACHINEREGEX).mli'$':R: '\1'.sled '\1'.rtl $LRTLHEAP
+^($MACHINEREGEX).ml'$' ($MACHINEREGEX).mli'$':R: '\1'.sled '\1'.rtl 
 	$LRTL -inst $stem1
 
-^($MACHINEREGEX)rec.ml'$':R: '\1'.sled '\1'.rtl $LRTLHEAP
+^($MACHINEREGEX)rec.ml'$':R: '\1'.sled '\1'.rtl 
 	$LRTL -recog4 $stem1
 
-^($MACHINEREGEX)rtl.ml'$':R: '\1'.sled '\1'.rtl $LRTLHEAP
+^($MACHINEREGEX)rtl.ml'$':R: '\1'.sled '\1'.rtl 
 	$LRTL -lower -qc--rtl $stem1
 
-^($MACHINEREGEX)mkasm.ml'$' ($MACHINEREGEX)mkasm.mli'$':R: '\1'.sled $LRTLHEAP
+^($MACHINEREGEX)mkasm.ml'$' ($MACHINEREGEX)mkasm.mli'$':R: '\1'.sled 
 	$LRTL -lower -emitasm $stem1
 
