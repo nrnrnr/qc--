336788cacff50799414b2ebef6ecf9119179fbc1
Christian Lindig <lindig@gmail.com>
2002-08-12 13:31:21 +0000

    lua.mk contains informations to find the Lua 4.0 libraries and header files.

---
+++ config/lua.mk
@@ -0,0 +1,19 @@
+#
+# Configuration for the Lua 4.0 C implementation, required for the Quick
+# C-- interpreter in interp/. Do not confuse with the Lua implementation
+# in OCaml in directory lua/.
+#
+
+# Add cc(1) compiler options to find the header file "lua.h". On Debian,
+# the header files are in /usr/inlcude/lua40.
+
+LUAINCLUDES=    -I/usr/include/lua40
+
+# Add cc(1) compiler options to link in the libraries liblua and
+# liblualib. The exact name of the libraries depend on your
+# installation. Often -llua and -llualib will be good enough. On Debian
+# libraries have the Lua version in their name
+
+LUALIBS=        -llua   -llualib
+LUALIBS=        -llua40 -llualib40
+
