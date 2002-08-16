5bd23c4c5e48dcfe2b7610dc4c396926667dca84
Norman Ramsey <nr@cs.tufts.edu>
2002-08-16 23:09:19 +0000

    many bug fixes and improvements
    **** COMPILES AND RUNS HELLO WORLD ****
    
    fails one test (unexpected Bits.Overflow in Rtlutil)

---
+++ test2/x86/hello.s
@@ -0,0 +1,25 @@
+.globl main
+.globl my_data
+.data
+/* memory for global registers */
+.align 8
+Cmm_private_global_area:
+.text
+.data
+my_data:
+.byte 104
+.byte 101
+.byte 108
+.byte 108
+.byte 111
+.byte 44
+.byte 32
+.byte 119
+.byte 111
+.byte 114
+.byte 108
+.byte 100
+.byte 33
+.byte 10
+.byte 0
+.text
