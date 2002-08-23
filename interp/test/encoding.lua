3ed10395f38d030f85dc413ddcf7b95987bce527
Reuben Olinsky <olinsky@post.harvard.edu>
2002-08-23 18:26:25 +0000

    new encoding test file; tests nearly all instructions in the interpreter's bytecode instruction set

--- interp/test/encoding.lua
+++ interp/test/encoding.lua
@@ -1,42 +1,111 @@
--- This code should not run.
--- It is only useful, when disassembled, to verify encoding.
---
---- ./regr "../../bin/qc--interp $SELF" $REGRFLAGS -out $BASE.1 -err $BASE.2
+CMM.memsize(8)
+CMM.ptrsize(32)
+CMM.byteorder("NATIVE")
+CMM.globals(300)
 
--- begin assembly code
+CMM.imports({ "cmmprint16" })
+CMM.exports({ "main", "przero" })
 
-CMM.exports({ "main", "main2" })
-CMM.imports({ "cmmprint32" })
-
-CMM.section("code")
-  CMM.procedure("main", 0, 0)
-    CMM.define_label("label2")
-      CMM.store_arg(0)
-      CMM.fetch_arg(0)
-      CMM.push_literal("0x1201230123", 32)
-      CMM.fetch_local(3)
-      CMM.store_local(5)
-      CMM.fetch_global(11)
-      CMM.store_global(12)
-      CMM.cmm_return(1, 2)
-      CMM.goto()
-      CMM.cbrancht("label2")
-      CMM.cbranchf("label3")
-      CMM.cut()
-    CMM.define_label("label3")
-      CMM.branchtf("label2", "label3");
-      CMM.cmm_tail_call()
-      CMM.c_call("printf", "char")
-      CMM.push_symbol("label2")
-      CMM.fetch(32, "LITTLE", 0)
-      CMM.store(32, "LITTLE", 0)
-      CMM.gstore(32, "LITTLE", 0)
-      CMM.gstore_local(5)
-      CMM.gstore_global(12)
-      CMM.call( {"label2"}, {}, {"label3"}, 1 )
+CMM.section("data")
+   CMM.align(4)
+ CMM.define_label("data1")
+   CMM.data(32, nil)
+   CMM.skip(4)
+   CMM.emit_raddr("main", 4)
+ CMM.define_label("GC_main")
 CMM.end_section()
 
-CMM.procedure("main2", 0, 0)
-  CMM.fetch_arg(0)
+CMM.section("text")
+ CMM.begin_span(1, "GC_main")
 
--- end of code
+   CMM.procedure("main", 5, 5)
+      CMM.define_label("label1")
+      CMM.comment("nothing-to-see-here")
+
+      CMM.push_symbol("label1")
+      CMM.push_literal("0x3", 32)
+      CMM.apply_operator("add", "bits32,bits32:bits32")
+      CMM.store_local(0)
+
+      CMM.fetch_local(0)
+      CMM.store_global(0)
+
+      CMM.fetch_global(0)
+      CMM.push_literal("0x1", 1)
+      CMM.gstore_global(1)
+
+      CMM.push_literal("0x1", 1)
+      CMM.push_symbol("data1")
+      CMM.fetch(32, "NATIVE", 0)
+      CMM.gstore_local(1)
+      CMM.push_symbol("data1")
+      CMM.fetch_local(1)
+      CMM.store(32, "NATIVE", 0)
+
+      CMM.push_literal("0x0", 1)
+      CMM.push_symbol("data1")
+      CMM.fetch_local(1)
+      CMM.gstore(32, "NATIVE", 0)
+
+      CMM.push_symbol("end1")
+      CMM.goto()
+      CMM.define_label("end1")
+
+      CMM.define_label("end2")
+      CMM.push_literal("0x0", 1)
+      CMM.branchtf("end2", "end3")
+
+      CMM.define_label("end3")
+      CMM.push_literal("0x1", 1)
+      CMM.cbrancht("end4")
+
+      CMM.define_label("end4")
+      CMM.push_literal("0x0", 1)
+      CMM.cbranchf("end5")
+
+      CMM.define_label("end5")
+
+      CMM.push_symbol("przero")
+      CMM.cmm_tail_call()
+
+      CMM.continuation("cont1")
+         CMM.fetch_arg(0)
+         CMM.store_arg(0)
+         CMM.cmm_return(0, 0)
+
+   CMM.end_procedure()
+
+   CMM.procedure("przero", 300, 2)
+      CMM.push_literal("0x44", 8)
+      CMM.store_local(280)
+
+      CMM.fetch_local(280)
+      CMM.store_global(280)
+
+      CMM.push_literal("0x0", 1)
+      CMM.fetch_global(280)
+      CMM.gstore_local(280)
+
+      CMM.push_literal("0x0", 1)
+      CMM.fetch_local(280)
+      CMM.gstore_global(280)
+      
+      CMM.push_literal("0x1ff", 16)
+      CMM.store_arg(0)
+      CMM.push_symbol("cmmprint16")
+
+      CMM.call({}, {}, {}, 0)
+
+      CMM.push_symbol("cmmprint16")
+      CMM.call({}, {}, {}, 1)
+
+      CMM.push_literal("0x1", 2)
+      CMM.store_local(10)
+
+      CMM.breakpt()
+      CMM.cmm_return(0, 0)
+   CMM.end_procedure()
+ CMM.end_span(1)
+CMM.end_section()
+
+CMM.new_assembly_unit()
