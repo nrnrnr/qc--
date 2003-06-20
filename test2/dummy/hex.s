61a2859979b8fd26c12959028f290c0a67da96d7
Norman Ramsey <nr@cs.tufts.edu>
2003-06-20 01:46:03 +0000

    new test case to tickle lexer bug

---
+++ test2/dummy/hex.s
@@ -0,0 +1,24 @@
+target
+    memsize 8
+    byteorder big
+    pointersize 32
+    wordsize 32
+    charset "latin1"
+    float "ieee754";
+
+export bits32 Cmm.globalsig.aQOYZWMPACZAJaMABGMOZeCCPY, notzero;
+
+section "data" { align 1; }
+
+section "data" { sym@Cmm.globalsig.aQOYZWMPACZAJaMABGMOZeCCPY: }
+
+section "data" { sym@Cmm.global_area: }
+
+section "data" { bits8[0::bits32]; }
+
+section "data" { sym@notzero: }
+
+section "data" { bits32[1::bits32] { a::bits32 }; }
+
+section "data" {  }
+
---
+++ test2/x86/hex.s
@@ -0,0 +1,9 @@
+.globl notzero
+.globl Cmm.globalsig.aQOYZWMPACZAJaMABGMOZeCCPY
+.data
+/* memory for global registers */
+Cmm.globalsig.aQOYZWMPACZAJaMABGMOZeCCPY:
+Cmm.global_area:
+.data
+notzero:
+.byte 10
