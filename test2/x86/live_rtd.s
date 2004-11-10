b9ca827d029846a911addd299f85ae1a62884622
John Dias <joaodias@gmail.com>
2004-11-10 22:49:46 +0000

    New test -- really added this time

---
+++ test2/x86/live_rtd.s
@@ -0,0 +1,48 @@
+.globl main
+.globl Cmm.global_area
+.globl Cmm.globalsig.aQOYZWMPACZAJaMABGMOZeCCPY
+.section .data
+/* memory for global registers */
+Cmm.globalsig.aQOYZWMPACZAJaMABGMOZeCCPY:
+Cmm.global_area:
+.section .text
+main:
+	leal -8(%esp), %esp
+	leal 8(%esp), %eax
+	movl (%eax),%eax
+Linitialize_continuations_l4:
+Lproc_body_start_l3:
+	movl $0,%edi
+	leal 8(%esp), %esi
+	movl $-8,%ebp
+	addl %ebp,%esi
+	movl %edi,(%esi)
+	movl %eax,4(%esp)
+	call exit
+Lpostcall_l7:
+.section .pcmap_data
+Lstackdata_l14:
+.long 0
+.section .pcmap
+.long Lpostcall_l7
+.long Lframe_l15
+.section .pcmap_data
+Lframe_l15:
+.long 0xfffffff8
+.long 0x80000004
+.long 0xfffffffc
+.long Lstackdata_l14
+.long 0x80000008
+.long 0x80000000
+.long 0x80000000
+.long 0x80000001
+.long 0x40000007
+.long 0
+.long 0x40000009
+.long 0
+.long 0x4000000a
+.long 0
+.long 0x4000000b
+.long 0
+.long 0x80000000
+.section .text
