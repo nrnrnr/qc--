db10ffd4a377e101a0d780805d6ca18d81021956
Norman Ramsey <nr@cs.tufts.edu>
2004-11-27 17:13:44 +0000

    a checkpoint in refactoring the run-time system stack walker. bugs may remain, but this RT is no worse than earlier versions

--- runtime/trace.c
+++ runtime/trace.c
@@ -28,9 +28,14 @@ void rt_stack_trace(Cmm_Cont* t) {
   printf("RTS: %X\n", (unsigned)t);
   a = Cmm_YoungestActivation(t);
   do {
+#if 0
+    Cmm_show_activation(&a);
+#endif
+    fflush(stdout);
     name = (char*)Cmm_GetDescriptor(&a, 1);
     printf("RTS: found activation \"%s\" pc:%X vfp:%X\n",
-           name, (unsigned)a.pc, (unsigned)a.vfp);
+           name, (unsigned)a.pc, (unsigned)a.vfpx);
+    fflush(stdout);
   } while (Cmm_ChangeActivation(&a));
 }
 
@@ -42,24 +47,35 @@ void rt_check(Cmm_Cont* t) {
   a = Cmm_YoungestActivation(t);
   do {
     void* rootp;
-    char* desc = (char*)Cmm_GetDescriptor(&a, 1);
-    int var_count = Cmm_LocalVarCount(&a);
-    int* sd = Cmm_FindStackLabel(&a, 0);
-    printf("Descriptor: %d \"%s\"\n", var_count, desc);
-    for (i = 0; i < var_count; i++) {
-      rootp = (void *) Cmm_FindLocalVar(&a, i);
-      if (rootp)
-        printf("\tlocal %d: %X = %X\n", i, (unsigned)rootp, *(int*)rootp);
-      else
-        printf("\tlocal %d: <DEAD>\n", i);
-    }
-
-    if (sd) {
-      for (i = 0; i < 3; i++) {
-        printf("\tsd %d: %X = %d\n", i, (unsigned)sd + i, sd[i]);
+    char* desc;
+    int var_count;
+    int* sd;
+#if 0
+    Cmm_show_activation(&a);
+#endif
+    fflush(stdout);
+    desc = (char*)Cmm_GetDescriptor(&a, 1);
+    var_count = Cmm_LocalVarCount(&a);
+    if (desc) {
+      sd = Cmm_FindStackLabel(&a, 0);
+      printf("Descriptor: %d \"%s\"\n", var_count, desc);
+      for (i = 0; i < var_count; i++) {
+        rootp = (void *) Cmm_FindLocalVar(&a, i);
+        if (rootp)
+          printf("\tlocal %d: %X = %X\n", i, (unsigned)rootp, *(int*)rootp);
+        else
+          printf("\tlocal %d: <DEAD>\n", i);
       }
-    }
 
+      if (sd) {
+        for (i = 0; i < 3; i++) {
+          printf("\tsd %d: %X = %d (%x)\n", i, (unsigned)sd + i, sd[i], sd[i]);
+        }
+      }
+    } else {
+      printf("Undescribed frame with %d locals\n", var_count);
+    }
+    fflush(stdout);
   } while (Cmm_ChangeActivation(&a));
 }
 
