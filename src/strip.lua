b070e7673163aadc48680c62b361dc15dd1e5c92
Norman Ramsey <nr@cs.tufts.edu>
2004-12-19 00:32:56 +0000

    onward and upward: built fragment of compiler, which is sufficient to run ./qc-- -v -S strip.lua hello.c-- and get some printed output

---
+++ src/strip.lua
@@ -0,0 +1,12 @@
+backend = backend or Backend.dummy
+
+backend.intwiden = nil
+backend.placevars = nil
+backend.floatwiden = nil
+backend.expand = nil
+backend.liveness = nil
+backend.ralloc =nil
+backend.freeze =nil
+backend.rmvfp = nil
+backend.asm= Asm.cmm
+backend.emit_data =nil
