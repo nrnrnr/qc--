72900470ea3199ccfb63e0b1f87451e81a130587
Norman Ramsey <nr@cs.tufts.edu>
2004-12-23 21:49:41 +0000

    combined live/dead optimization works on a simple example

--- src/strip.lua
+++ src/strip.lua
@@ -1,10 +1,10 @@
 backend = backend or Backend.dummy
 
 backend.intwiden = nil
-backend.placevars = nil
+backend.placevars = Placevar.context(Placevar.x86, "x86")
 backend.floatwiden = nil
 backend.expand = nil
-backend.liveness = nil
+backend.liveness = Liveness.liveness
 backend.ralloc =nil
 backend.freeze =nil
 backend.rmvfp = nil
@@ -12,3 +12,8 @@ backend.asm= Asm.cmm
 backend.emit_data =nil
 
 backend.preopt = Optimize.remove_nops
+
+--backend.placevars = nil
+backend.liveness =nil
+
+Options.validate = nil
