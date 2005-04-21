15a1b4fafddd5adb04afe993c758a1a884e43baa
Norman Ramsey <nr@cs.tufts.edu>
2005-04-21 21:47:50 +0000

    more tests

--- lua/test/n00.lua
+++ lua/test/n00.lua
@@ -21,11 +21,25 @@ function h(x, y, z)
   return x, y, z
 end
 
+function i(x)
+  return x, f()
+end
+
+function j(x)
+  return f(), x
+end
+
 x, y, z = h(3, f())
 assert(x == 3 and y == 1 and z == 2)
 
 x, y, z = h(f(), 3)
 assert(x == 1 and y == 3 and z == nil)
+
+x, y, z = i(99)
+assert(x == 99 and y == 1 and z == 2)
+
+x, y, z = j(99)
+assert(x == 1 and y == 99 and z == nil)
 
 
 print('OK')
