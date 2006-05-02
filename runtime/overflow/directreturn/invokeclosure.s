cc8368884461d39cdff3e30d7ad381150d5ec98e
Daniel Peng <danielpeng@gmail.com>
2006-05-02 05:01:55 +0000

    Preliminary demo of underflow handling without intervening return continuation. Registers should be handled properly; the next iteration will test overflow blocks.

--- runtime/overflow/directreturn/invokeclosure.s
+++ runtime/overflow/directreturn/invokeclosure.s
@@ -1,8 +1,11 @@
 .globl invokeclosure_takes3_returns2
+.globl abstract_memory_size
+.section .data
+abstract_memory_size: .word 12
 
 .section .text
 invokeclosure_takes3_returns2:
-        leal -28(%esp), %esp
+        leal -70(%esp), %esp
 	/* We never return, so we don't need to save the base pointer. */
 
 .Linitialize_continuations_l14:
@@ -16,27 +19,57 @@ invokeclosure_takes3_returns2:
 	
         movl %eax,%ecx
 	/* 0(%ecx) is returns_to.  not used here. */
-        movl 4(%ecx),%ebp  /* function (f) */
-        movl 8(%ecx),%eax  /* argument 1 (a) */
-        movl 12(%ecx),%esi /* argument 2 (b) */
-        movl 16(%ecx),%edi /* argument 3 (c) */
+        movl 16(%ecx),%ebp  /* function (f) */
+        movl 20(%ecx),%eax  /* argument 1 (a) */
+        movl 24(%ecx),%esi /* argument 2 (b) */
+        movl 28(%ecx),%edi /* argument 3 (c) */
         leal -8(%esp), %esp
         movl %esi,(%esp)
         movl %edi,4(%esp)
         call *%ebp
 .Lcall_successor_l17:
-        movl %edx,8(%esp)   /* return value 2 (y) */
+/* save abstract machine state */
+	movl %eax,30(%esp)
+	movl %ebx,34(%esp)
+/* we don't need to save ECX.	movl %ecx,38(%esp) */
+	movl %edx,42(%esp)
+	movl %esi,46(%esp)
+	movl %edi,50(%esp)
+	movl %ebp,54(%esp)
+	movl %esp,58(%esp)
+
+/* copy overflow block */
+	movl (%esp), %eax  /* load closure */
+	movl 8(%eax), %edi /* dst overflow block */
+	leal 4(%esp), %esi    /* src overflow block */
+	movl 12(%eax), %ecx /* overflow block size */
+	std
+	rep movsd
+	
+/* now we can call functions or whatever */
+
+/* freestack cut */
         movl (%esp),%edx    /* argument 1 (closure) */
         leal 20(%esp), %ebx /* argument 2 (k) */
-        movl %eax,16(%esp)  /* return value 1 (x) */
-
         movl freestack,%eax
         movl 4(%eax), %esp; jmp *(%eax)
 .Lcut_entry_l11:
 .Lstart_of_continuation_code_l10:
-        movl 8(%esp),%ebx   /* argument 2 (y) */
-        movl 16(%esp),%edx  /* argument 1 (x) */
-
+/* ok now we're going to modify the saved machine state */
         movl (%esp),%eax  /* loads closure */
-	movl (%eax),%eax  /* loads returns_to */
-        movl 4(%eax), %esp; jmp *(%eax)
+	movl 4(%eax),%ecx  /* old SP */
+	movl %ecx,58(%esp) /* replace new SP */
+
+	movl (%eax),%ecx /* old RA.  we get to keep this. */
+	
+/* restore machine state */
+	movl 30(%esp),%eax
+	movl 34(%esp),%ebx
+/* we don't need to save ECX.	movl %ecx,38(%esp) */
+	movl 42(%esp),%edx
+	movl 46(%esp),%esi
+	movl 50(%esp),%edi
+	movl 54(%esp),%ebp
+	movl 58(%esp),%esp
+
+	jmp *%ecx
