5086a26c188d7421ebd0531694f535340368bf7a
Daniel Peng <danielpeng@gmail.com>
2006-05-10 22:28:01 +0000

    Fixed the bug: > Say f overflows.  When invokeclosure > returns directly to f's caller, it doesn't restore any callee-save registers > that were saved in f's frame before it cut to the overflow handler.
    We give invokeclosure a 'body' continuation for the body of f.  Then, it
    can give this to the runtime system and ask it where callee-save registers
    in f are stored.  This turns out to be surprisingly painless with Norman's
    help.
    
    I have checked that callee-save registers are restored out of the same
    memory locations that they are saved to.
    
    I haven't been able to test that callee-save registers are actually preserved;
    qc-- by default doesn't want to allocate my variables in callee-save registers.
    With backend.ralloc=Ralloc.color it will, but then my code and the runtime system
    crash, depending on which files are compiled with which register coloring algorithm.

--- runtime/overflow/directreturn/runtime-internals.c
+++ runtime/overflow/directreturn/runtime-internals.c
@@ -47,3 +47,45 @@ void* getOverflowBlock(Cmm_Cont* k) {
   printf("getOverflowBlock %x\n", (Cmm_Codeptr*)locptr(&a, a.rtdata->inalloc ));
   return (Cmm_Codeptr*)locptr(&a, a.rtdata->inalloc );
 }
+
+struct absmem {
+  Cmm_Word eax, ecx, edx, ebx, ebp, esi, edi, esp;
+};
+
+void get_callee_save_registers(struct absmem* state, Cmm_Cont* body) {
+  int i;
+  Cmm_Activation callee_a = Cmm_YoungestActivation(body);
+  
+  struct reg* regs = registers(callee_a.rtdata);
+  
+  for(i = 0; i < callee_a.rtdata->num_registers; i++) {
+    Cmm_Word value = *(Cmm_Word*)locptr(&callee_a, regs[i].saved);
+    switch (regs[i].index) {
+      case 4: /* eax volatile; used for first result */
+        assert(0);
+	break; 
+      case 5: /* ecx volatile */
+        assert(0); 
+	break;
+      case 6: /* edx volatile; used for second result */
+        assert(0); 
+        break;
+      case 7: /* ebx nonvolatile */
+        state->ebx = value;
+        break;
+      case 8:/* esp special cased. */
+        printf("Got esp %x\n", value);
+        break;
+      case 9: /* ebp nonvolatile */
+        state->ebp = value;
+        break;
+      case 10:/* esi nonvolatile */
+	state->esi = value;
+        break;
+      case 11:/* edi nonvolatile */
+        state->edi = value;
+        break;
+    }
+  }
+  return;
+}
